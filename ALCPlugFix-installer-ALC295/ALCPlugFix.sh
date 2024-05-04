a9241287a9fc377a624c68916997e8b0f40e1cc8
Lorys89 <64613690+Lorys89@users.noreply.github.com>
2024-05-04 16:39:01 +0200

    Update ALCPlugFix.sh

--- ALCPlugFix-installer-ALC295/ALCPlugFix.sh
+++ ALCPlugFix-installer-ALC295/ALCPlugFix.sh
@@ -2,8 +2,7 @@
 
 
 /usr/local/bin/alc-verb 0x20 SET_COEF_INDEX 0x45
-/usr/local/bin/alc-verb 0x20 SET_PROC_COEF 0x5289
+/usr/local/bin/alc-verb 0x20 SET_PROC_COEF 0xE689
 
 /usr/local/bin/alc-verb 0x19 SET_PIN_WIDGET_CONTROL 0x24
 /usr/local/bin/alc-verb 0x21 SET_UNSOLICITED_ENABLE 0x83
-/usr/local/bin/alc-verb 0x12 SET_PIN_WIDGET_CONTROL 0x22
