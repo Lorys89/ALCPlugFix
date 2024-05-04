49e95f04dc2e1e765a3bfa64030b815244850b2b
Lorys89 <64613690+Lorys89@users.noreply.github.com>
2024-05-04 19:46:57 +0200

    Update ALCRebootFromWinFix.sh

--- ALCPlugFix-installer-ALC295/ALCRebootFromWinFix.sh
+++ ALCPlugFix-installer-ALC295/ALCRebootFromWinFix.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-# Fix for no sound from internal speakers when rebooting from Windows
-
-/usr/local/bin/alc-verb 0x20 SET_COEF_INDEX 0x10
-/usr/local/bin/alc-verb 0x20 SET_PROC_COEF 0x0120
-
-/usr/local/bin/alc-verb 0x20 SET_COEF_INDEX 0x0d
-/usr/local/bin/alc-verb 0x20 SET_PROC_COEF 0xa023
-
-
 # Fix for no mic and headphone jack switching when rebooting from Windows
 
 /usr/local/bin/alc-verb 0x20 SET_COEF_INDEX 0x45
 /usr/local/bin/alc-verb 0x20 SET_PROC_COEF 0x5289
-
-/usr/local/bin/alc-verb 0x20 SET_COEF_INDEX 0x46
-/usr/local/bin/alc-verb 0x20 SET_PROC_COEF 0x0004
-
