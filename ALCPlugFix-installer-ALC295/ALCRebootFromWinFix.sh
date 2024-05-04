f4b408bc8ac3b582ec55bd9ced82db21ea91699f
Lorys89 <64613690+Lorys89@users.noreply.github.com>
2024-05-04 15:54:32 +0200

    Update ALCRebootFromWinFix.sh

--- ALCPlugFix-installer-ALC295/ALCRebootFromWinFix.sh
+++ ALCPlugFix-installer-ALC295/ALCRebootFromWinFix.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Fix for no mic and headphone jack switching when rebooting from Windows
-
 /usr/local/bin/alc-verb 0x20 SET_COEF_INDEX 0x45
 /usr/local/bin/alc-verb 0x20 SET_PROC_COEF 0x5289
-
