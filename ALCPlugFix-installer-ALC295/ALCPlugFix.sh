1a5d274a8e94193a2819d7285b390054ea70e6dd
Lorys89 <64613690+Lorys89@users.noreply.github.com>
2024-05-06 09:35:18 +0200

    Update ALCPlugFix.sh

--- ALCPlugFix-installer-ALC295/ALCPlugFix.sh
+++ ALCPlugFix-installer-ALC295/ALCPlugFix.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
 
 
-# Fixing issues when rebooting from Windows
-
-/usr/local/bin/ALCRebootFromWinFix.sh
-
-
-# Headphone static fix
-
-/usr/local/bin/ALCHeadphoneStaticFix.sh
-
-
 # Headphone jack plug/unplug fix
 
 /usr/local/bin/alc-verb 0x19 SET_PIN_WIDGET_CONTROL 0x24
-
-
-
-
-
-
-
-
